a3085eca582b354dbdcf121f577492e4c857df57
kartikkpawar <knpwar789@gmail.com>
2024-12-28 22:46:06 +0530

    feat: footer and big-text section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React, { Fragment } from "react";
 import SkyDive from "./__components/SkyDive";
 import CansCarousel from "./__components/CansCarousel";
 import AlternateText from "./__components/AlternateText";
+import BigText from "./__components/BigText";
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
       <SkyDive />
       <CansCarousel />
       <AlternateText />
+      <BigText />
     </Fragment>
   );
 }
