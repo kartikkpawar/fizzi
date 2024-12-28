4c11e9bedac132912dc5a41425bff1a97cf5b3c7
kartikkpawar <knpwar789@gmail.com>
2024-12-28 22:32:10 +0530

    feat: alternate-text section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/app/__components/Hero";
 import React, { Fragment } from "react";
 import SkyDive from "./__components/SkyDive";
 import CansCarousel from "./__components/CansCarousel";
+import AlternateText from "./__components/AlternateText";
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
       <Hero />
       <SkyDive />
       <CansCarousel />
+      <AlternateText />
     </Fragment>
   );
 }
