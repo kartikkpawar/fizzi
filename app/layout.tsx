d2096c05d98711d897b81a93126c26d23138ca87
kartikkpawar <knpwar789@gmail.com>
2024-12-28 10:57:54 +0530

    chore: project init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,11 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
   weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  variable: "--font-alpino",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={alpino.variable} suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-alpino)", "sans-serif"],
+      },
+      keyframes: {
+        "slide-left": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "slide-left": "slide-left 3s linear infinite",
+        "spin-slow": "spin 6s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 export default config;
